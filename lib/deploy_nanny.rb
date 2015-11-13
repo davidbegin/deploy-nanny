83e326dd4a013db852079e3be1901a5333f34b58
David Begin <davidmichaelbe@gmail.com>
2015-11-12 21:59:37 -0800

    move options_parse into its own class

--- lib/deploy_nanny.rb
+++ lib/deploy_nanny.rb
@@ -16,24 +16,7 @@ module DeployNanny
                 environments:,
                 deploy_instructions:)
 
-      options = {}
-      OptionParser.new do |opts|
-        opts.banner = "\nUsage: bin/your_exec [options]\n".yellow
-
-        opts.on("-n", "--no-deploy", "Do not auto-deploy out-of-date apps") do
-          options[:no_deploy] = true
-        end
-
-				opts.on("-s", "--sleep=SLEEP", "Time to sleep between sweeps in minutes.") do |sleep|
-          options[:sleep] = sleep.to_i * 60
-				end
-
-        opts.on_tail("-h", "--help", "Here are all the options Deploy Nanny takes") do
-          puts opts
-          exit
-        end
-      end.parse!
-      @options = OpenStruct.new(options)
+      @options = OpenStruct.new(parse_options)
 
       babysitter = Base.new(
         github_account: github_account,
@@ -60,6 +43,27 @@ module DeployNanny
 
     def sweep_rest
       @options.sleep || 60 * 5
+    end
+
+    def parse_options
+      options = {}
+      OptionParser.new do |opts|
+        opts.banner = "\nUsage: bin/your_exec [options]\n".yellow
+
+        opts.on("-n", "--no-deploy", "Do not auto-deploy out-of-date apps") do
+          options[:no_deploy] = true
+        end
+
+        opts.on("-s", "--sleep=SLEEP", "Time to sleep between sweeps in minutes.") do |sleep|
+          options[:sleep] = sleep.to_i * 60
+        end
+
+        opts.on_tail("-h", "--help", "Here are all the options Deploy Nanny takes") do
+          puts opts
+          exit
+        end
+      end.parse!
+      options
     end
 
   end
